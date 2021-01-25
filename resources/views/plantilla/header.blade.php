29816752711123bb56c528cf207cf70fd4ec8767
Alam Lopez <ulises.lopez@grupodisatel.com>
2021-01-25 11:05:00 -0600

    Corrigiendo Auth nombre

--- resources/views/plantilla/header.blade.php
+++ resources/views/plantilla/header.blade.php
@@ -10,7 +10,7 @@
     <ul class="nav navbar-nav ml-auto">
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-                <span class="d-md-down-none">{{ Auth::user()->nombre }}</span>
+                <span class="d-md-down-none">Opciones</span>
             </a>
             <div class="dropdown-menu dropdown-menu-right">
                 <div class="dropdown-header text-center">
