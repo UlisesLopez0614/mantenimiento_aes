48c77457c0bc45b9c0ec0d4533f1c3ea102e669a
FSHK <farseerhk@gmail.com>
2021-04-21 10:22:29 -0600

    HotFix 0.1 Programador de Tareas

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        'App\Console\Commands\SendAlerts'
     ];
 
     /**
@@ -27,36 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call(function () {
-
-            $Vehicles = DB::table('tb_principal')
-                ->select('tb_vehicles.Name','tb_vehicles.Plate','tb_vehicles.Fleet','tb_vehicles.Area','tb_vehicles.odo_actual','tb_mtto_history.kms_goal', 'tb_mtto_history.mtto_count','tb_mtto_history.correo','tb_mtto_history.correo_supervisor','tb_principal.quedan')
-                ->join('tb_vehicles','tb_principal.FK_idVehicle','=','tb_vehicles.id')
-                ->join('tb_mtto_history','tb_principal.FK_idMtto','=','tb_mtto_history.id')
-                ->where('tb_vehicles.estado','=',1)
-                ->where('tb_principal.quedan','<',480)
-                ->get();
-            foreach ($Vehicles as $item)
-            {
-                $VeH = $item->Name."-".$item->Plate." / ".$item->Fleet." ".$item->Area;
-                if($item->quedan > 300)
-                {
-                    $correos = collect(explode(";", $item->correo));
-                    foreach($correos as $key => $value)
-                    {
-                        Mail::to($value)->queue(new AlertasPruebaAES($VeH,$item->quedan));
-                    }
-                }
-                else
-                {
-                    $correo_sup = explode(";", $item->correo_supervisor);
-                    foreach($correo_sup as $key => $value)
-                    {
-                        Mail::to($value)->queue(new AlertasPruebaAES($VeH,$item->quedan));
-                    }
-                }
-            }
-        })->twiceDaily();
+        $schedule->command('command:alerts')->twiceDaily(11,4);
     }
 
     /**
